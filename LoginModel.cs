1d931acd4f0a34a76517c4dd04f446958c43017d
GulencMehmet <mehmet@nabusoft.com>
2026-07-08 14:03:56 +0300

    refactor: rename LoginModel to LoginRequestModel

--- LoginModel.cs
+++ LoginModel.cs
@@ -1,6 +1,6 @@
 namespace GithubDemoApp;
 
-public class LoginModel
+public class LoginRequestModel
 {
     public string Username { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
