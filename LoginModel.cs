2a713931dd9f102433e7e194a72885153ee176f4
GulencMehmet <mehmet@nabusoft.com>
2026-07-08 14:14:02 +0300

    feat: add TcNo property to LoginRequestModel

--- LoginModel.cs
+++ LoginModel.cs
@@ -2,6 +2,7 @@ namespace GithubDemoApp;
 
 public class LoginRequestModel
 {
+    public string TcNo { get; set; }
     public string Email { get; set; }
     public string Username { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
